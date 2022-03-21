4a6f09899d40a167e22d4c275aee851b360ca77b
hppav <hppav@DESKTOP-T60V25J>
2022-03-21 22:33:48 +0100

    Добавьте файлы проекта.

---
+++ Ex.6/Ex.6.cpp
@@ -0,0 +1,59 @@
+﻿// Ex.6
+#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(0, "RUS");
+	cout << " Ёлочка\n" << "\n";
+	cout << " Введите количество ярусов ёлочки:\n";
+	int a;
+	cin >> a;
+	cout << " Введите высоту яруса ёлочки:\n";
+	int b;
+	cin >> b;
+
+	int spaces = a * b / 2 - 1;
+	int stars = 1;
+	int Z = spaces;
+
+	for (int i = 0; i < a; i++) // ярусы
+	{
+		int X = stars; 
+		int Y = spaces;
+
+		for (int y = 0; y < b; y++) // строки
+		{
+
+				for (int s = 0; s < Y; s++)
+				{
+					cout << " ";
+				}
+				for (int x = 0; x < X; x++)
+				{
+					cout << "*"; 
+				}
+				cout << "\n";
+
+					Y--; 
+					X += 2; 
+		}
+		stars += 2;
+		spaces--;
+	}
+	
+
+	for (int y = 0; y < a; y++) // ствол 
+	{
+		
+			for (int s = 0; s < Z - 1; s++)
+			{
+				cout << " ";
+			}
+
+			for (int x = 0; x < a; x++)
+			{
+				cout << "*";
+			}
+			cout << "\n";
+	}
+}
